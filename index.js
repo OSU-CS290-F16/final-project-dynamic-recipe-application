edc550baaae6fc2e093b86724017b8b0c6440a2a
Kevin Stine <ikaikastine@gmail.com>
2016-12-04 20:53:00 -0800

    added ability to dynamically insert new ingredient and added green submit button

--- index.js
+++ index.js
@@ -13,5 +13,45 @@ $(function(){
 });
 
 
+var addIngredient = document.getElementById('addButton');
+addIngredient.addEventListener('click', addNewIngredient);
+
+
+
+
+function addNewIngredient(event) {
+  var currentIngredient = document.getElementById('addIngredient');
+  console.log("ooo I got clicked");
+  var newIngredient = document.createElement('div');
+  newIngredient.className = 'row newRowSpace';
+  newIngredient.innerHTML =
+    '<div class="col-lg-6">\
+      <div class="input-group">\
+        <span class="input-group-addon" id="basic-addon1">Ingredient</span>\
+        <input type="text" class="form-control" placeholder="ex. flour" aria-describedby="basic-addon1">\
+      </div>\
+      <!-- /input-group -->\
+    </div>\
+    <div class="col-lg-6">\
+      <div class="input-group">\
+        <span class="input-group-addon" id="basic-addon1">Amount</span>\
+        <input type="text" class="form-control" placeholder="ex. 5" aria-describedby="basic-addon1" aria-label="...">\
+        <div class="input-group-btn">\
+          <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Select Units <span class="caret"></span></button>\
+          <ul class="dropdown-menu dropdown-menu-right">\
+            <li><a>Cups</a></li>\
+            <li><a>Another action</a></li>\
+            <li><a>Something else here</a></li>\
+            <li role="separator" class="divider"></li>\
+            <li><a>Separated link</a></li>\
+          </ul>\
+        </div>\
+        <!-- /btn-group -->\
+      </div>\
+      <!-- /input-group -->\
+    </div>';
+
+    currentIngredient.appendChild(newIngredient);
+}
 
 console.log("I work!");
