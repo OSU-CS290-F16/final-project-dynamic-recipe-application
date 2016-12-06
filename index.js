a338ebbdf6cdacff9b2ae43e3328ded6c0fc8d0d
Kevin Stine <ikaikastine@gmail.com>
2016-12-06 13:36:24 -0800

    fixed add ingredient button from having its text changed

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
-function dropdown(val) {
-  var y = document.getElementsByClassName('btn btn-default dropdown-toggle');
-  var aNode = y[0].innerHTML = val + ' <span class="caret"></span>'; // Append
-}
-
 $(function(){
 
     $(".dropdown-menu").on('click', 'li a', function(){
-      $(".btn:first-child").text($(this).text());
-      $(".btn:first-child").val($(this).text());
+      $(".btn-units:first-child").text($(this).text());
+      $(".btn-units:first-child").val($(this).text());
    });
 
 });
